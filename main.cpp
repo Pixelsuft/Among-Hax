175eed0ff06a6539afc9823d028991b124d95ccf
pixelsuft <68371847+Pixelsuft@users.noreply.github.com>
2020-12-20 15:56:28 +0700

    Test Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <iostream>
+#include <cstdlib>
 #include <vector>
+bool no_cooldown_active=false;
 //Made by PixelSuft
 BOOL writeMemory(uintptr_t const address, std::vector<uint8_t> const& bytes){
         return WriteProcessMemory(GetCurrentProcess(), reinterpret_cast<LPVOID>(address), bytes.data(), bytes.size(), nullptr);
@@ -11,8 +13,26 @@ DWORD WINAPI main(LPVOID lpParam){
         FILE* f;
         freopen_s(&f, "CONOUT$", "w", stdout);
         SetConsoleTitleA("Pixelsuft Among Hax");
-        if(writeMemory(base + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
-                std::cout <<"Ok!" <<std::endl;
+        while(true){
+                if(no_cooldown_active==false)cout <<"1)No cooldown (off)" <<endl;
+                else cout <<"1)No cooldown (on)" <<endl;
+                
+                cout <<"Your choose: ";
+                int choose=0;
+                cin >> choose;
+                
+                if(choose==1){
+                        if(no_cooldown_active==false){
+                                if(writeMemory(base + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
+                                        std::cout <<"Activated!" <<std::endl;
+                        }
+                        else{
+                                if(writeMemory(base + 0xE4955A, { 0xF3, 0x0F, 0x11, 0x46, 0x44 }))
+                                        std::cout <<"Deactivated!" <<std::endl;
+                        }
+                }
+                else break;
+        }
         FreeConsole();
         return TRUE;
 }
