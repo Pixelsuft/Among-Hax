b2788e78bfccc0df42ec495cbe6e2133d0a5653e
pixelsuft <68371847+Pixelsuft@users.noreply.github.com>
2020-12-21 16:27:00 +0700

    Test Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <Windows.h>
 #include <iostream>
-#include <cstdlib>
 #include <vector>
 using namespace std;
-bool no_cooldown_active=false;
 //Made by PixelSuft
 BOOL writeMemory(uintptr_t const address, std::vector<uint8_t> const& bytes){
         return WriteProcessMemory(GetCurrentProcess(), reinterpret_cast<LPVOID>(address), bytes.data(), bytes.size(), nullptr);
@@ -13,28 +11,17 @@ DWORD WINAPI main(LPVOID lpParam){
         AllocConsole();
         FILE* f;
         freopen_s(&f, "CONOUT$", "w", stdout);
+	setlocale(0,"");
         SetConsoleTitleA("Pixelsuft Among Hax");
-        setlocale(0,"");
-        while(true){
-                if(no_cooldown_active==false)cout <<"1)No cooldown (off)" <<endl;
-                else cout <<"1)No cooldown (on)" <<endl;
-                
-                cout <<"Your choose: ";
-                int choose=0;
-                cin >> choose;
-                
-                if(choose==1){
-                        if(no_cooldown_active==false){
-                                if(writeMemory(base + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
-                                        std::cout <<"Activated!" <<std::endl;
-                        }
-                        else{
-                                if(writeMemory(base + 0xE4955A, { 0xF3, 0x0F, 0x11, 0x46, 0x44 }))
-                                        std::cout <<"Deactivated!" <<std::endl;
-                        }
-                }
-                else break;
-        }
+	system("color 0a");
+	cout <<"Pixelsuft Among Hax" <<endl <<"Created by Pixelsuft" <<endl <<endl;
+	if(FileExists("no_cooldown.txt")){
+		if(writeMemory(base + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
+			cout <<"No cooldown [on]" <<endl;
+		else cout <<"No cooldown [error]" <<endl;
+	}
+	else cout <<"No cooldown [off]" <<endl;
+	cout <<endl <<"Now you can close window";
         FreeConsole();
         return TRUE;
 }
