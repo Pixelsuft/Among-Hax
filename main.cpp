34ee02c41a17a7c354a60041617d91db9869cf9a
pixelsuft <68371847+Pixelsuft@users.noreply.github.com>
2020-12-23 10:59:38 +0700

    Test Update main.cpp
    Add imposter beta, no cool down

--- main.cpp
+++ main.cpp
@@ -6,22 +6,40 @@ using namespace std;
 BOOL writeMemory(uintptr_t const address, std::vector<uint8_t> const& bytes){
         return WriteProcessMemory(GetCurrentProcess(), reinterpret_cast<LPVOID>(address), bytes.data(), bytes.size(), nullptr);
 }
-DWORD base=(DWORD)GetModuleHandleA("GameAssembly.dll");
+DWORD game_a=(DWORD)GetModuleHandleA("GameAssembly.dll");
+DWORD up=(DWORD)GetModuleHandleA("UnityPlayer.dll");
 DWORD WINAPI main(LPVOID lpParam){
         AllocConsole();
         FILE* f;
         freopen_s(&f, "CONOUT$", "w", stdout);
-	setlocale(0,"");
+		setlocale(0,"");
         SetConsoleTitleA("Pixelsuft Among Hax");
-	system("color 0a");
-	cout <<"Pixelsuft Among Hax" <<endl <<"Created by Pixelsuft" <<endl <<endl;
-	if(FileExists("no_cooldown.txt")){
-		if(writeMemory(base + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
-			cout <<"No cooldown [on]" <<endl;
-		else cout <<"No cooldown [error]" <<endl;
-	}
-	else cout <<"No cooldown [off]" <<endl;
-	cout <<endl <<"Now you can close window";
+		system("color 0a");
+		cout <<"Pixelsuft Among Hax" <<endl <<"Created by Pixelsuft" <<endl <<endl;
+		if(FileExists("no_cooldown.txt")){
+			if(writeMemory(game_a + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
+				cout <<"No cooldown [on]" <<endl;
+			else cout <<"No cooldown [error]" <<endl;
+		}
+		else cout <<"No cooldown [off]" <<endl;
+		if(FileExists("noclip.txt")){
+			if(writeMemory(up + 0x960ca6, { 0x85 }))
+				cout <<"Noclip [on]" <<endl;
+			else cout <<"Noclip [error]" <<endl;
+		}
+		else cout <<"Noclip [off]" <<endl;
+		//Importer is beta
+		if(FileExists("imposter.txt")){
+			uint64_t Imposter = RPM<uint64_t>(game_a + 0x144BB70);
+			Imposter = RPM<uint64_t>(Imposter + 0x5C);
+			Imposter = RPM<uint64_t>(Imposter + 0x0);
+			Imposter = RPM<uint64_t>(Imposter + 0x34);
+			WPM<int>(Imposter + 0x28, 1);
+			cout <<"Imposter [on]" <<endl;
+		}
+		else cout <<"Imposter [off]" <<endl;
+		
+		cout <<endl <<"Now you can close window";
         FreeConsole();
         return TRUE;
 }
