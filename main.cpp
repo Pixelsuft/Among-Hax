a4af9d40416727cb1c93c6186576976c0a593dc7
pixelsuft <68371847+Pixelsuft@users.noreply.github.com>
2020-12-29 16:14:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <Windows.h>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
-#include <WTypesbase.h>  //beta
-#include <cstdint>  //beta
+//#include <WTypesbase.h>  //beta
+//#include <cstdint>  //beta
 
 using namespace std;
 //Made by PixelSuft
@@ -25,31 +26,68 @@ DWORD WINAPI main(LPVOID lpParam){
 		setlocale(0,"");
         SetConsoleTitleA("Pixelsuft Among Hax");
 		system("color 0a");
-		cout <<"Pixelsuft Among Hax" <<endl <<"Created by Pixelsuft" <<endl <<endl;
 		bool no_cooldown_on=false;
 		bool noclip_on=false;
+		bool was_changed=true; //true for first display options
 		while(true)
 		{
-		if(GetAsyncKeyState(VK_F1))
-		{
-		if(no_cooldown_on){
-			if(writeMemory(game_a + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
-				cout <<"No cooldown [on]" <<endl;
-			else cout <<"No cooldown [error]" <<endl;
+			if(GetAsyncKeyState(VK_F1))
+			{
+				if(no_cooldown_on==false){
+					if(writeMemory(game_a + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
+					{
+						no_cooldown_on=true;
+						was_changed=true;
+					}
+				}
+				else
+				{
+					if(writeMemory(game_a + 0xE4955A, { 0x90, 0x90, 0x90, 0x90, 0x90 }))
+					{
+						no_cooldown_on=false;
+						was_changed=true;
+					}
+				}
+			}
+			if(GetAsyncKeyState(VK_F2))
+			{
+				if(noclip_on==false){
+					if(writeMemory(up + 0x960ca6, { 0x85 }))
+					{
+						noclip_on=true;
+						was_changed=true;
+					}
+				}
+				else
+				{
+					if(writeMemory(up + 0x960ca6, { 0x84 }))
+					{
+						noclip_on=false;
+						was_changed=true;
+					}
+				}
+			}
+			if(GetAsyncKeyState(VK_ESCAPE))
+			{
+				break;
+			}
+			if(was_changed)
+			{
+				was_changed=false;
+				system("cls");
+				cout <<"Pixelsuft Among Hax" <<endl <<"Created by Pixelsuft" <<endl <<endl;
+				cout <<"No Cooldown [F1] ";
+				if(no_cooldown_on==true)cout <<"[ON]";
+				else cout <<"[OFF]";
+				cout <<";" <<endl;
+				cout <<"NoClip [F2] ";
+				if(noclip==true)cout <<"[ON]";
+				else cout <<"[OFF]";
+				cout <<";" <<endl;
+				cout <<"Break [ESC]" <<";" <<endl;
+			}
 		}
-		else cout <<"No cooldown [off]" <<endl;
-		}
-		if(GetAsyncKeyState(VK_F1))
-		{
-		if(noclip_on){
-			if(writeMemory(up + 0x960ca6, { 0x85 }))
-				cout <<"Noclip [on]" <<endl;
-			else cout <<"Noclip [error]" <<endl;
-		}
-		else cout <<"Noclip [off]" <<endl;
-		}
-		}
-		cout <<endl <<"Now you can close this window";
+		cout <<endl <<endl <<"Now you can close this window";
         FreeConsole();
         return TRUE;
 }
